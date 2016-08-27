61d200372d6bf6611a0988e674b2e4caa2c280c7
ldesrosi <ldesrosi@uk.ibm.com>
2016-08-27 17:52:43 +0100

    update to motors.sh

--- iot-tank/motors.sh
+++ iot-tank/motors.sh
@@ -1,2 +1,12 @@
+#!/bin/bash 
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f "$0")
+
+# Absolute path this script is in, thus /home/user/bin
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+cd $SCRIPTPATH/..
 git pull git@github.com:ldesrosi/iot-tank.git
+
+cd iot-tank
 mvn exec:java -Dexec.mainClass="com.ibm.iot.tank.App" -Dexec.classpathScope=runtime 
