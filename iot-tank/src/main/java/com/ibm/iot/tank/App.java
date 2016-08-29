da30e2d534be168b8292f3c97b9f83a40abc64d3
ldesrosi <ldesrosi@uk.ibm.com>
2016-08-29 19:08:11 +0100

    Fix iot startup

--- iot-tank/src/main/java/com/ibm/iot/tank/App.java
+++ iot-tank/src/main/java/com/ibm/iot/tank/App.java
@@ -1,5 +1,6 @@
 package com.ibm.iot.tank;
 
+import com.ibm.iot.comms.IoTManager;
 import com.ibm.iot.motor.MotorException;
 import com.ibm.iot.tank.controller.BasicTankController;
 import com.ibm.iot.tank.controller.IoTTankController;
@@ -8,11 +9,13 @@ import com.ibm.iot.tank.controller.TankController;
 public class App {
 	public static void main(String[] args) {
 		try {
+			//Start by connecting to IoT before starting any other thread.
+			IoTManager.getManager();
+			
 			final Tank tank = new Tank();
 			final TankController controller = new IoTTankController(tank);
 			
-			Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
-				
+			Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {			
 				@Override
 				public void run() {
 					try {
