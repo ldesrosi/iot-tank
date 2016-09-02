f1c3202a562ebd074f713c5ed25a7d41a11da28a
ldesrosi <ldesrosi@uk.ibm.com>
2016-09-03 00:18:00 +0100

    added PiCamera files directly in project

--- openwhisk-distance-action/src/main/java/com/ibm/openwhisk/tank/ManageDistance.java
+++ openwhisk-distance-action/src/main/java/com/ibm/openwhisk/tank/ManageDistance.java
@@ -10,11 +10,14 @@ public class ManageDistance {
     	tankCommand.addProperty("deviceId", input.getAsJsonPrimitive("deviceId").getAsString());
     	tankCommand.addProperty("sessionId", input.getAsJsonPrimitive("sessionId").getAsLong());
 
-    	if (distance < 5) {
-    		tankCommand.addProperty("command", "turnLeft");    		
-    	} else if (distance >= 5 && distance < 10) {
+    	if (distance < 10) {
+    		tankCommand.addProperty("command", "turnLeft");    
+    		tankCommand.addProperty("speed", 60);
+    	} else if (distance >= 10 && distance < 15) {
+    		tankCommand.addProperty("command", "moveForward");
         	tankCommand.addProperty("speed", 60);
     	} else {
+    		tankCommand.addProperty("command", "moveForward");
         	tankCommand.addProperty("speed", 100);
     	}
 
