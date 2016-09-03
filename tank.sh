482a17560cddec36ce1f74c9d7caf8fea291c2a0
ldesrosi <ldesrosi@uk.ibm.com>
2016-09-03 18:27:36 +0100

    c

--- tank.sh
+++ tank.sh
@@ -1,17 +1,16 @@
 #!/bin/bash 
 # Absolute path to this script, e.g. /home/user/bin/foo.sh
 SCRIPT=$(readlink -f "$0")
-
 # Absolute path this script is in, thus /home/user/bin
 SCRIPTPATH=$(dirname "$SCRIPT")
 
 cd $SCRIPTPATH/../JRPiCam
 git pull git@github.com:ldesrosi/JRPiCam.git
-sudosudo env "PATH=$PATH" 
+sudo env "PATH=$PATH" mvn install -DskipTests
+
 cd $SCRIPTPATH/
 git pull git@github.com:ldesrosi/iot-tank-client.git
 sudo env "PATH=$PATH"  mvn install -DskipTests
 
 cd $SCRIPTPATH/iot-tank
-
 sudo env "PATH=$PATH" mvn exec:java -DskipTests -Dexec.mainClass="com.ibm.iot.tank.App" -Dexec.classpathScope=runtime 
