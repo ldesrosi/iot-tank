44525403db6c021a78d915d51280af3399ce62a1
ldesrosi <ldesrosi@uk.ibm.com>
2016-09-03 16:58:40 +0100

    Directory reorg

--- openwhisk-tank-action/src/main/resources/openWhisk.sh
+++
@@ -1,7 +0,0 @@
-wsk trigger create sessionStarted
-wsk trigger create movementUpdate
-wsk trigger create turnComplete
-wsk package create tank
-wsk action create tank/createSession tank-action-0.0.1-SNAPSHOT.jar 
-wsk action create tank/move tank-distance-action-0.0.1-SNAPSHOT.jar
-wsk action create tank/convertToPayload utils-0.0.1-SNAPSHOT.jar 
